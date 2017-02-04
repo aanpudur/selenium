6682dfa6860ecd2f17932e2db9fc6150c20dd88c
aanpudur <aanpudur@gmail.com>
2017-02-04 04:45:51 -0800

    HW

--- Documents/workspace1/Webdriver/src/module1/HW1.java
+++ Documents/workspace1/Webdriver/src/module1/HW1.java
@@ -6,28 +6,28 @@ public class HW1 {
 		// TODO Auto-generated method stub
 		
 		int days = 7;
+		double dollar = 7.86;
+		char c = 'I';
+		String name = "Aan Vazhapilly";
+		byte dayoff = 2;
+		short appsinstall = 10;
+		int pagesRead = 10;
+		
 		
 		System.out.println("Week has " + days + " days");
 		
-		double dollar = 7.86;
-		System.out.println("Each Pizza Slices is " + dollar + " Dollars");
-	
-		char c = 'I';
+		System.out.println("Each Pizza Slices is " + dollar + " Dollars");	
+		
 		System.out.println("First letter of india is \'" + c + "\'");
 		
-		String name = "Aan Vazhapilly";
 		System.out.println("My name is " + name);
 		
-		byte dayoff = 2;
 		System.out.println("I took " + dayoff + " days off");
-		
 		
 		System.out.println("length of my name is " + name.length() + " letters");
 		
-		short appsinstall = 10;
 		System.out.println(appsinstall + " apps installed on my phone");
 		
-		int pagesRead = 10;
 		System.out.println("So far i have read " + pagesRead + " pages");
 		
 		
