d3d607b6a4d62fae1d33845708db4204c1878efa
aanpudur <aanpudur@gmail.com>
2017-02-04 20:52:44 -0800

    Sum of n numbers

---
+++ Documents/workspace1/Webdriver/src/module1/HW3_sumOfNumbers.java
@@ -0,0 +1,20 @@
+package module1;
+
+//Find the sum of first N natural number ( 1- 100)
+
+public class HW3_sumOfNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum = 0;
+		
+		for(int n = 0; n <=100; n++){
+			
+			sum += n;
+		}
+		
+		System.out.println(sum);
+
+	}
+
+}
